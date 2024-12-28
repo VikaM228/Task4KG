9f9df3cb54af3aba33bbaed0e5ac39b28390dc8b
MariyaS228 <mariya.syrceva2020@gmail.com>
2024-12-28 19:43:56 +0300

    final

--- src/main/java/com/cgvsu/Main.java
+++ src/main/java/com/cgvsu/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Simple3DViewer.main(args);
     }
-}
+}//222222222
+//мой проект + викин
+//cnfhssq llm
+//ehf ehf
+//бебебе
